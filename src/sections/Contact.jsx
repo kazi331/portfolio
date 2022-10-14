6eaf61692ebd6221514a3558a8f2a569ca09156b
kazi331 <kazisharifulislam52@gmail.com>
2022-10-14 08:55:28 +0600

    mobile contact menu added

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -66,15 +66,15 @@ const Contact = () => {
           <form onSubmit={sendEmail} ref={form} className="leading-7 text-sm">
             <div className="relative mb-4">
               <label htmlFor="name">Name</label>
-              <input type="text" id="name" name="name" className="w-full bg-transparent rounded ring-1 ring-indigo-400 focus:ring-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required />
+              <input type="text" id="name" name="name" className="w-full bg-transparent rounded ring-0 sm:ring-1 border border-indigo-400 sm:border-none ring-indigo-400  focus:ring-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required />
             </div>
             <div className="relative mb-4">
               <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" className="w-full bg-transparent rounded ring-1 ring-indigo-400 focus:ring-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required />
+              <input type="email" id="email" name="email" className="w-full bg-transparent rounded ring-0 sm:ring-1 border border-indigo-400 sm:border-none ring-indigo-400  focus:ring-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required />
             </div>
             <div className="relative mb-4">
               <label htmlFor="message">Message</label>
-              <textarea id="message" name="message" className="w-full bg-transparent rounded ring-1 ring-indigo-400 focus:ring-indigo-500 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required></textarea>
+              <textarea id="message" name="message" className="w-full bg-transparent rounded ring-0 sm:ring-1 border border-indigo-400 sm:border-none ring-indigo-400  focus:ring-indigo-500 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required></textarea>
             </div>
             <button className=" w-full bg-indigo-500 bg-opacity-10  border-2 border-indigo-700 py-2 px-6 focus:outline-none hover:bg-indigo-600 hover:bg-opacity-40 rounded text-center">Send Message</button>
           </form>
