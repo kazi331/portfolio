fb0af2aa62fd082e9061a91d7192772a9d8b90cf
Kazi Shariful Islam <96835101+kazi331@users.noreply.github.com>
2023-11-01 09:17:02 +0600

    about image updated

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-scroll';
-import me from '../resources/black-profile.jpg';
+import me from '../resources/sharif_black.jpg';
 import { githubIcon, GithubLink, linkedinIcon, mailIcon, whatsappIcon } from './SocialIcons';
 
 
